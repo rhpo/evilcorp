c9ba921ad299bdf0906cf888c943b9fa6e12269b
badisso <mehdi.moussous2162@gmail.com>
2026-02-22 21:40:27 +0100

    Update AttackForm.Designer.cs

--- EvilCorp/AttackForm.Designer.cs
+++ EvilCorp/AttackForm.Designer.cs
@@ -29,45 +29,59 @@ namespace EvilCorp
             // lblTarget
             //
             this.lblTarget.AutoSize = true;
+            this.lblTarget.Font = new System.Drawing.Font("Inter", 10F);
+            this.lblTarget.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(180)))), ((int)(((byte)(200)))), ((int)(((byte)(230)))));
             this.lblTarget.Location = new System.Drawing.Point(20, 20);
             this.lblTarget.Name = "lblTarget";
-            this.lblTarget.Size = new System.Drawing.Size(74, 15);
+            this.lblTarget.Size = new System.Drawing.Size(74, 17);
             this.lblTarget.TabIndex = 0;
             this.lblTarget.Text = "Target User";
             //
             // cmbTargetUser
             //
+            this.cmbTargetUser.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(40)))), ((int)(((byte)(60)))));
             this.cmbTargetUser.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbTargetUser.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cmbTargetUser.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(240)))), ((int)(((byte)(255)))));
             this.cmbTargetUser.Location = new System.Drawing.Point(20, 45);
             this.cmbTargetUser.Name = "cmbTargetUser";
-            this.cmbTargetUser.Size = new System.Drawing.Size(300, 23);
+            this.cmbTargetUser.Size = new System.Drawing.Size(300, 25);
             this.cmbTargetUser.TabIndex = 1;
             //
             // lblAttackType
             //
             this.lblAttackType.AutoSize = true;
+            this.lblAttackType.Font = new System.Drawing.Font("Inter", 10F);
+            this.lblAttackType.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(180)))), ((int)(((byte)(200)))), ((int)(((byte)(230)))));
             this.lblAttackType.Location = new System.Drawing.Point(20, 85);
             this.lblAttackType.Name = "lblAttackType";
-            this.lblAttackType.Size = new System.Drawing.Size(66, 15);
+            this.lblAttackType.Size = new System.Drawing.Size(80, 17);
             this.lblAttackType.TabIndex = 2;
             this.lblAttackType.Text = "Attack Type";
             //
             // cmbAttackType
             //
+            this.cmbAttackType.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(40)))), ((int)(((byte)(60)))));
             this.cmbAttackType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbAttackType.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cmbAttackType.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(240)))), ((int)(((byte)(255)))));
             this.cmbAttackType.Location = new System.Drawing.Point(20, 110);
             this.cmbAttackType.Name = "cmbAttackType";
-            this.cmbAttackType.Size = new System.Drawing.Size(300, 23);
+            this.cmbAttackType.Size = new System.Drawing.Size(300, 25);
             this.cmbAttackType.TabIndex = 3;
             //
             // btnStart
             //
+            this.btnStart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(100)))), ((int)(((byte)(200)))));
+            this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStart.FlatAppearance.BorderSize = 0;
+            this.btnStart.ForeColor = System.Drawing.Color.White;
             this.btnStart.Location = new System.Drawing.Point(20, 150);
             this.btnStart.Name = "btnStart";
             this.btnStart.Size = new System.Drawing.Size(300, 40);
             this.btnStart.TabIndex = 4;
             this.btnStart.Text = "Start";
-            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.UseVisualStyleBackColor = false;
             this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
             //
             // progress
@@ -80,6 +94,8 @@ namespace EvilCorp
             // lblStatus
             //
             this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Inter", 9F);
+            this.lblStatus.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(180)))), ((int)(((byte)(200)))), ((int)(((byte)(230)))));
             this.lblStatus.Location = new System.Drawing.Point(20, 235);
             this.lblStatus.Name = "lblStatus";
             this.lblStatus.Size = new System.Drawing.Size(0, 15);
@@ -89,7 +105,8 @@ namespace EvilCorp
             //
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(350, 280);
+            this.ClientSize = new System.Drawing.Size(360, 280);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(35)))), ((int)(((byte)(55)))));
             this.Controls.Add(this.lblTarget);
             this.Controls.Add(this.cmbTargetUser);
             this.Controls.Add(this.lblAttackType);
@@ -101,7 +118,7 @@ namespace EvilCorp
             this.MaximizeBox = false;
             this.Name = "AttackForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "Attack Interface (Simulation)";
+            this.Text = "Attack Interface (Demo)";
             this.ResumeLayout(false);
             this.PerformLayout();
         }
