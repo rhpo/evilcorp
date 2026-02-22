6235d40540b00d2433a16a0f5578528a4142ff9c
badisso <mehdi.moussous2162@gmail.com>
2026-02-22 18:07:34 +0100

    Initial commit

---
+++ EvilCorp/LoginForm.cs
@@ -0,0 +1,48 @@
+namespace EvilCorp
+{
+    public partial class LoginForm : Form
+    {
+        public User? AuthenticatedUser { get; private set; }
+
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text.Trim();
+            string password = txtPassword.Text;
+
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                lblError.Text = "Please enter both username and password.";
+                return;
+            }
+
+            User? user = DatabaseManager.AuthenticateUser(username, password);
+
+            if (user != null)
+            {
+                AuthenticatedUser = user;
+                this.DialogResult = DialogResult.OK;
+                this.Close();
+            }
+            else
+            {
+                lblError.Text = "Invalid username or password.";
+                txtPassword.Clear();
+            }
+        }
+
+        private void btnLogin_MouseEnter(object sender, EventArgs e)
+        {
+            btnLogin.BackColor = System.Drawing.Color.FromArgb(60, 120, 220);
+        }
+
+        private void btnLogin_MouseLeave(object sender, EventArgs e)
+        {
+            btnLogin.BackColor = System.Drawing.Color.FromArgb(50, 100, 200);
+        }
+    }
+}
