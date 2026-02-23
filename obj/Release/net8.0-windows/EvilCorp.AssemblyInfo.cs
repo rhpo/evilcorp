1af706545394572ff69ab34de182ea6d31f044df
badisso <mehdi.moussous2162@gmail.com>
2026-02-23 13:45:25 +0100

    Bump AssemblyInformationalVersion and caches
    Update AssemblyInformationalVersion from 1.0.0+761cea7c5cbf2e582e864748f79db8a55b7b5179 to 1.0.0+13a1aee563afd11748153a455259c700f159b4a3 in Debug and Release obj/AssemblyInfo.cs files, and refresh the corresponding AssemblyInfoInputs.cache hashes in obj/Debug and obj/Release. These are autogenerated build artifact updates reflecting a new informational version.

--- obj/Release/net8.0-windows/EvilCorp.AssemblyInfo.cs
+++ obj/Release/net8.0-windows/EvilCorp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EvilCorp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+761cea7c5cbf2e582e864748f79db8a55b7b5179")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13a1aee563afd11748153a455259c700f159b4a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("EvilCorp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EvilCorp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
