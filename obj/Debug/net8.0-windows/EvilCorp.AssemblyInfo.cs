6fde6f072646e18d35e4a6ed4f4323216ec30eda
badisso <mehdi.moussous2162@gmail.com>
2026-02-23 13:51:21 +0100

    um....

--- obj/Debug/net8.0-windows/EvilCorp.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/EvilCorp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EvilCorp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13a1aee563afd11748153a455259c700f159b4a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca9144d9dfa1ca32b38df074e2d6b77b4eb2b520")]
 [assembly: System.Reflection.AssemblyProductAttribute("EvilCorp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EvilCorp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
